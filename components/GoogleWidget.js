0d1f6cbad01f831c67b5d7f19fa08235344f543c
Thomas Augot <thomas.augot@hotmail.fr>
2024-01-30 19:25:09 +0100

    fixing google button

--- components/GoogleWidget.js
+++ components/GoogleWidget.js
@@ -23,7 +23,7 @@ function GoogleWidget() {
         href="https://www.google.com/search?sca_esv=601739216&rlz=1C1ONGR_frFR1029FR1030&tbm=lcl&sxsrf=ACQVn0-fWAj5KZ3_YMpjnAs56s2pPg6k0w:1706281887129&q=Charpente+Menuiserie+Durand+Reviews&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxI2NLSwsDC1NLIwszAzMTM0NDA12MDI-IpR2TkjsaggNa8kVcE3Na80szi1KDNVwaW0KDEvRSEotSwztbx4ESsxqgAd6xn9ZgAAAA&rldimm=1188859286864611050&hl=en-FR&sa=X&ved=2ahUKEwjahpOuq_uDAxVmbKQEHTqqAiEQ9fQKegQIRBAF&biw=1366&bih=607&dpr=1#lkt=LocalPoiReviews&lrd=0x480f89d613b78025:0x107fad38f97d46ea,3,,,,"
         target="_blank"
         rel="noreferrer"
-        className="text-white text-lg bg-darkGrey p-4 rounded-lg transition hover:scale-95 text-center"
+        className="secondary-button secondary-button-bg"
       >
         Donnez votre avis!
       </a>
