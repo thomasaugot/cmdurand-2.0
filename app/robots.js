8c4ec01057eab4e77c8e305f9ea9040031f87de0
Thomas Augot <thomas.augot@hotmail.fr>
2025-08-25 09:59:08 +0100

    fix SEO robots.txt config

--- app/robots.js
+++ app/robots.js
@@ -1,35 +1,36 @@
 export default function robots() {
-  const baseUrl = 'https://www.cmdurand.fr/'
+  const baseUrl = "https://www.cmdurand.fr/";
 
   // Production rules
   return {
     rules: [
       // General rules for all search engines
       {
-        userAgent: '*',
-        allow: '/',
+        userAgent: "*",
+        allow: "/",
         disallow: [
-          '/api/',           // Block API endpoints
-          '/admin/',         // Block admin areas  
-          '/_next/',         // Block Next.js internals
-          '/private/',       // Block private content
+          "/api/", // Block API endpoints
+          "/admin/", // Block admin areas
+          "/_next/", // Block Next.js internals
+          "/private/", // Block private content
         ],
-        crawlDelay: 1,       // Be respectful to server
+        crawlDelay: 1, // Be respectful to server
       },
-      
+
       // Give Google special treatment (no crawl delay)
       {
-        userAgent: 'Googlebot',
-        allow: '/',
-        disallow: [
-          '/api/',
-          '/admin/',
-          '/_next/',
-          '/private/',
-        ],
+        userAgent: "Googlebot",
+        allow: "/",
+        disallow: ["/api/", "/admin/", "/_next/", "/private/"],
+      },
+
+      // Optional: Block bandwidth-hungry SEO bots
+      {
+        userAgent: ["AhrefsBot", "SemrushBot", "MJ12bot", "DotBot"],
+        disallow: "/",
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
     host: baseUrl,
-  }
+  };
 }
